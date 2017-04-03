d56dba68efc3b8ba6dee41f8d668bafcc17c6980
Matt Crowe <github.mpc@zym.me>
2017-04-03 19:30:25 +0000

    places with sections

--- resources/js/components/places/routes.js
+++ resources/js/components/places/routes.js
@@ -1,17 +1,19 @@
 import index from './ctlr/index';
 import create from './ctlr/create';
 import edit  from './ctlr/edit';
+import amenities  from './ctlr/amenities';
 import categories  from './ctlr/categories';
 import attachments  from './ctlr/attachments';
+import sections  from './ctlr/sections';
 import tags  from './ctlr/tags';
-import amenities  from './ctlr/amenities';
 
 export default [
     {path: '/places', component: index, canReuse: false, name: 'places'},
     {path: '/places/create', component: create, name: 'places.create'},
     {path: '/places/edit/:id', component: edit, name: 'places.edit'},
+    {path: '/places/edit/:id/amenities', component: amenities, name: 'places.amenities'},
     {path: '/places/edit/:id/attachments', component: attachments, name: 'places.attachments'},
     {path: '/places/edit/:id/categories', component: categories, name: 'places.categories'},
+    {path: '/places/edit/:id/sections/:section?', component: sections, name: 'places.sections'},
     {path: '/places/edit/:id/tags', component: tags, name: 'places.tags'},
-    {path: '/places/edit/:id/amenities', component: amenities, name: 'places.amenities'},
 ]
