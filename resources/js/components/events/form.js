07989ac23e45d1cb1c83b5caec50f54a58800203
rob.lasota <rob.lasota@fahlgren.com>
2017-04-04 12:47:07 -0400

    updating ends at and starts at

--- resources/js/components/events/form.js
+++ resources/js/components/events/form.js
@@ -1,5 +1,6 @@
 import BaseForm from 'belt/core/js/helpers/form';
 import BaseService from 'belt/core/js/helpers/service';
+import moment from 'moment';
 
 class eventForm extends BaseForm {
 
@@ -32,15 +33,15 @@ class eventForm extends BaseForm {
         super.setData(data);
 
         if (this.starts_at) {
-            let starts_at = new Date(this.starts_at);
-            this.starts_at_date = dateformat(starts_at, "yyyy-mm-dd");
-            this.starts_at_time = dateformat(starts_at, "HH:MM");
+            let starts_at = moment(this.starts_at);
+            this.starts_at_date = starts_at.format("YYYY-MM-DD");
+            this.starts_at_time = starts_at.format("HH:MM");
         }
 
         if (this.ends_at) {
-            let ends_at = new Date(this.ends_at);
-            this.ends_at_date = dateformat(ends_at, "yyyy-mm-dd");
-            this.ends_at_time = dateformat(ends_at, "HH:MM");
+            let ends_at = moment(this.ends_at);
+            this.ends_at_date = ends_at.format("YYYY-MM-DD");
+            this.ends_at_time = ends_at.format("HH:MM");
         }
     }
 
