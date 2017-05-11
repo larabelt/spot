b9c0207b9b6ac667aa639bab211e36e14117e868
rob.lasota <rob.lasota@fahlgren.com>
2017-05-11 07:45:37 -0400

    updated the shared mixin to have the form

--- resources/js/components/itineraries/ctlr/shared.js
+++ resources/js/components/itineraries/ctlr/shared.js
@@ -3,17 +3,22 @@ import heading_html from 'belt/core/js/templates/heading.html';
 import tabs_html from '../templates/tabs.html';
 import edit_html from '../templates/edit.html';
 
+import Form from '../form';
+
 export default {
     data() {
         return {
             morphable_type: 'itineraries',
             morphable_id: this.$route.params.id,
-            itinerary: this.$parent.itinerary,
+            itinerary: new Form(),
         }
     },
     components: {
         heading: {template: heading_html},
         tabs: {template: tabs_html},
+    },
+    mounted() {
+        this.itinerary.show(this.morphable_id);
     },
     template: edit_html,
 }
