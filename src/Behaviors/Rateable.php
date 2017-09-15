8dd2d73db6fede8e14b62a34a7e65be6a789ad0e
Matt Crowe <github.mpc@zym.me>
2017-09-15 19:58:35 +0000

    rateable mutator

--- src/Behaviors/Rateable.php
+++ src/Behaviors/Rateable.php
@@ -1,9 +1,16 @@
 <?php
+
 namespace Belt\Spot\Behaviors;
 
 trait Rateable
 {
 
-
+    /**
+     * @param $value
+     */
+    public function setRatingAttribute($value)
+    {
+        $this->attributes['rating'] = $value ? number_format($value, 2) : null;
+    }
 
 }
