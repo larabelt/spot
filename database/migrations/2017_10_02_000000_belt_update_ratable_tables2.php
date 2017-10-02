943ec77222ced7d668c2ad729e4a7a3a0011d98a
Matt Crowe <github.mpc@zym.me>
2017-10-02 20:42:10 +0000

    index for rateable tables

---
+++ database/migrations/2017_10_02_000000_belt_update_ratable_tables2.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeltUpdateRatableTables2 extends Migration
+{
+
+    /**
+     * @var array
+     */
+    private $tables = ['deals', 'events', 'itineraries', 'places'];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->tables as $table_name) {
+            Schema::table($table_name, function (Blueprint $table) {
+                $table->index('rating');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->tables as $table_name) {
+            Schema::table($table_name, function (Blueprint $table) use ($table_name) {
+                $table->dropIndex($table_name . '_rating_index');
+            });
+        }
+    }
+}
