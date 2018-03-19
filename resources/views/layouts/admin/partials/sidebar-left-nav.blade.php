fe1273e32f99490a0233521375bac73bff401a2a
Matt Crowe <github.mpc@zym.me>
2018-03-19 14:39:10 +0000

    sidebar nav ids

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -7,25 +7,25 @@
 @endphp
 
 @if($team || $can['amenities'] || $can['deals'] || $can['events'] || $can['itineraries'] || $can['places'])
-    <li class="treeview">
+    <li id="spot-admin-sidebar-left" class="treeview">
         <a href="#">
             <i class="fa fa-globe"></i> <span>Spot Admin</span> <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
             @if($can['amenities'])
-                <li><a href="/admin/belt/spot/amenities"><i class="fa fa-plug"></i> <span>Amenities</span></a></li>
+                <li id="spot-admin-sidebar-left-amenities"><a href="/admin/belt/spot/amenities"><i class="fa fa-plug"></i> <span>Amenities</span></a></li>
             @endif
             @if($team || $can['deals'])
-                <li><a href="/admin/belt/spot/deals"><i class="fa fa-usd"></i> <span>Deals</span></a></li>
+                <li id="spot-admin-sidebar-left-deals"><a href="/admin/belt/spot/deals"><i class="fa fa-usd"></i> <span>Deals</span></a></li>
             @endif
             @if($team || $can['events'])
-                <li><a href="/admin/belt/spot/events"><i class="fa fa-calendar"></i> <span>Events</span></a></li>
+                <li id="spot-admin-sidebar-left-events"><a href="/admin/belt/spot/events"><i class="fa fa-calendar"></i> <span>Events</span></a></li>
             @endif
             @if($can['itineraries'])
-                <li><a href="/admin/belt/spot/itineraries"><i class="fa fa-map-marker"></i> <span>Itineraries</span></a></li>
+                <li id="spot-admin-sidebar-left-itineraries"><a href="/admin/belt/spot/itineraries"><i class="fa fa-map-marker"></i> <span>Itineraries</span></a></li>
             @endif
             @if($team || $can['places'])
-                <li><a href="/admin/belt/spot/places"><i class="fa fa-building"></i> <span>Places</span></a></li>
+                <li id="spot-admin-sidebar-left-places"><a href="/admin/belt/spot/places"><i class="fa fa-building"></i> <span>Places</span></a></li>
             @endif
         </ul>
     </li>
