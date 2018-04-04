8260975ff04a8afed7f39dfdc9fa86afa7a606bb
Matt Crowe <github.mpc@zym.me>
2018-04-04 20:17:45 +0000

    sections UX continued

--- resources/belt/spot/js/places/edit/sections/index.js
+++ resources/belt/spot/js/places/edit/sections/index.js
@@ -1,5 +1,5 @@
 import edit from 'belt/spot/js/places/edit/shared';
-import sections from 'belt/content/js/sectionables/ctlr/index';
+import sections from 'belt/content/js/sectionables/router';
 
 export default {
     mixins: [edit],
