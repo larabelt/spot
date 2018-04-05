ce14d5a7db3a0eab082c393a3ce329b133951c26
Matt Crowe <github.mpc@zym.me>
2018-04-05 20:29:57 +0000

    tests

--- tests/Functional/Web/WebItinerariesFunctionalTest.php
+++ tests/Functional/Web/WebItinerariesFunctionalTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Belt\Core\Testing;
+use Belt\Spot\Itinerary;
 
 class WebItinerariesFunctionalTest extends Testing\BeltTestCase
 {
@@ -10,9 +11,19 @@ class WebItinerariesFunctionalTest extends Testing\BeltTestCase
         $this->refreshDB();
         $this->actAsSuper();
 
+        Itinerary::unguard();
+        $itinerary = Itinerary::find(1);
+        $itinerary->update(['is_active' => true]);
+
         # show
         $response = $this->json('GET', '/itineraries/1');
         $response->assertStatus(200);
+
+        $itinerary->update(['is_active' => false]);
+
+        # show (404)
+        $response = $this->json('GET', '/itineraries/1');
+        $response->assertStatus(404);
     }
 
 }
