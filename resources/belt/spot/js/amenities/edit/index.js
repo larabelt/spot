c55f55560d8b3b279ab4969188cdd96e86d7b97f
Matt Crowe <github.mpc@zym.me>
2018-05-16 19:48:34 +0000

    breadcrumb updates

--- resources/belt/spot/js/amenities/edit/index.js
+++ resources/belt/spot/js/amenities/edit/index.js
@@ -1,10 +1,9 @@
 import form from 'belt/spot/js/amenities/form';
-
 import html from 'belt/spot/js/amenities/edit/template.html';
 
 export default {
+    mixins: [form],
     components: {
-
         edit: form,
     },
     template: html,
