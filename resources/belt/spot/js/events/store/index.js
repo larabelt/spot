3b967b8bda885c4352d54785d181ae6fca6fba50
Matt Crowe <github.mpc@zym.me>
2018-05-30 14:36:16 +0000

    vuejs: places/events store config

--- resources/belt/spot/js/events/store/index.js
+++ resources/belt/spot/js/events/store/index.js
@@ -23,6 +23,8 @@ export default {
             return new Promise((resolve, reject) => {
                 state.form.show(eventID)
                     .then(response => {
+                        dispatch('config/set', {configKey: response.template});
+                        dispatch('config/load');
                         resolve(response);
                     })
                     .catch(error => {
