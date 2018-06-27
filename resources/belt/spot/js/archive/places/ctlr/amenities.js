5cab8e12e2f8b0d0f8a00b749a8fb54a9f093f32
Matt Crowe <github.mpc@zym.me>
2018-06-27 18:47:15 +0000

    rm archived files

--- resources/belt/spot/js/archive/places/ctlr/amenities.js
+++
@@ -1,9 +0,0 @@
-import shared from 'belt/spot/js/places/ctlr/shared';
-import amenities from 'belt/spot/js/amenity-spots/list';
-
-export default {
-    mixins: [shared],
-    components: {
-        tab: amenities,
-    },
-}
