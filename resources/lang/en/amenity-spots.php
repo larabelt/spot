db7face167b8b6bbeda3327fb9e014d562568384
Matt Crowe <github.mpc@zym.me>
2018-09-17 19:31:43 +0000

    language/localization updates

--- resources/lang/en/amenity-spots.php
+++ resources/lang/en/amenity-spots.php
@@ -3,5 +3,6 @@
 return [
     'manager' => [
         'overall' => "Manage your item's amenities.",
+        'description' => "Select or complete the amenities you want linked to your item.",
     ],
 ];
