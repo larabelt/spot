4bf856180d05b44968b91aaba86500839dde3757
Matt Crowe <github.mpc@zym.me>
2019-02-12 19:37:18 +0000

    continue unified documentation

--- resources/views/docs/20/admin/spot/places/intro.blade.php
+++ resources/views/docs/20/admin/spot/places/intro.blade.php
@@ -11,49 +11,77 @@ this instance, we are creating a restaurant
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/place-creator-subtype.png'])
 
-* **Is Active** ​​If this is selected, your content will automatically show up on the Dominica Site.
-* **Name** ​​Jens Island Eats
-* **Phone Number**​​ Enter valid number here. Formatting will show up as it is entered in CMS on site
-* **Toll Free Phone**​​ leave blank.
-* **Website**​​ Add website with http:// or https://:​ ​http://website.com
-* **Email** Add Email
-* **Hours** Add Hours text
-* **Meta Title** will be page title and will show up on tab in browser
-* **Met Description** this is for SEO purposes and shows up in the site's search results
-* **Meta Keywords​​** Add keywords for SEO purposes if you desire
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Is Active', 'Check this box to make the Place publicly available.'],
+        ['Name', '​​Jens Island Eats'],
+        ['Phone Number', 'Enter valid number here. Formatting will show up as it is entered in CMS on site.'],
+        ['Toll Free Phone', '_Leave Blank_'],
+        ['Website', 'Add website with http:// or https://:​ ​http://website.com'],
+        ['Email', 'Add Email'],
+        ['Hours', 'Add Hours text'],
+        ['Meta Title', 'Will be page title and will show up on tab in browser.'],
+        ['Meta Description', 'This is for SEO purposes and shows up in the site\'s search results.'],
+        ['Meta Keywords​​', 'Add keywords for SEO purposes if you desire.'],
+    ],
+])
 
 Click on Save. Additional fields and tabs will become available.
 
-* **Hero Image**​​ Link an image you wish to use for the main page image. You will need to add the image to the attachments tab or section prior to uploading it in the Main Tab.
-* **Heading**​​ Leave Blank
-* **Hero Heading**​​ This is the main page heading
-* **Intro Copy**​​ Shows up under Hero Image and before Body
-* **Body**​​ Body copy of page
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Hero Image', 'Link an image you wish to use for the main page image. You will need to add the image to the attachments tab or section prior to uploading it in the Main Tab.'],
+        ['Heading', '_Leave Blank_'],
+        ['Hero Heading', 'This is the main page heading'],
+        ['Intro Copy', 'Shows up under Hero Image and before Body'],
+        ['Body', 'Body copy of page'],
+    ],
+])
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/place-detail-example.png'])
 
-* **Accommodation Details** Ignore for restaurants, more useful for Accommodations ex. hotels
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Accommodation Details', 'Ignore for restaurants, more useful for Accommodations ex. hotels'],
+    ],
+])
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/param-group-accommodation-detail.png'])
 
-* **Island Map Settings** controls the default map that appears near the bottom of the page on all Places. This map can be hidden by switching "Show Island Map" to "Hide."
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Island Map Settings', 'Controls the default map that appears near the bottom of the page on all Places. This map can be hidden by switching "Show Island Map" to "Hide."'],
+    ],
+])
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/param-group-island-map.png'])
 
-* **Island Map Heading** Headline shows above map
-* **Island Map Intro** Intro text shows above map
-* **Island Map** choose the theme for your island map
-* **Show Island Map** Show or hide the island map from your Place.
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Island Map Heading', 'Headline shows above map'],
+        ['Island Map Intro', 'Intro text shows above map'],
+        ['Island Map', 'Choose the theme for your island map'],
+        ['Show Island Map', 'Show or hide the island map from your Place.'],
+    ],
+])
 
 Front End view of Map section:
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/map-example.png'])
 
-* **Big Image Link** Add a link to other sections of the site. Shows up at bottom of page.
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Big Image Link', 'Add a link to other sections of the site. Shows up at bottom of page.'],
+    ],
+])
 
 @include('belt-docs::partials.image', ['src' => '20/admin/spot/assets/param-big-image-link-adventure.png'])
 
-* **Candid Tag** Choose theme for Instagram photos that show up on your Place page.
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Candid Tag', 'Choose theme for Instagram photos that show up on your Place page.'],
+    ],
+])
 
 #### Locations tab:
 
